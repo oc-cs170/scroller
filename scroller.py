6c994b72e81230fa4aa2dc6f555e1a97dbc032a6
Marcus M. Darden <marcus.darden@cs.olivetcollege.edu>
2013-03-05 20:08:50 -0500

    World drawn, hero added.

--- scroller.py
+++ scroller.py
@@ -4,11 +4,15 @@
 """
 
 import pygame
+import hero
+
 
 WINDOW_TITLE = 'Scroller'
 WINDOW_WIDTH = 1024
 WINDOW_HEIGHT = 768
 FPS = 30
+
+WORLD_WIDTH = 5 * WINDOW_WIDTH
 
 
 class Scroller(object):
@@ -20,8 +24,23 @@ class Scroller(object):
         self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
                                               # pygame.FULLSCREEN)
 
+        pygame.key.set_repeat(10, 10)
+
         # Use a clock to control frame rate
         self.clock = pygame.time.Clock()
+
+        self.dirt_block = pygame.image.load('Dirt Block.png').convert_alpha()
+
+        self.world = pygame.Surface((WORLD_WIDTH, WINDOW_HEIGHT))
+        self.world.fill((pygame.Color('Sky Blue')))
+        x = 0
+        while x < WORLD_WIDTH:
+            self.world.blit(self.dirt_block, (x, 514))
+            self.world.blit(self.dirt_block, (x, 598))
+            x += 101
+        self.vp = [0, 0]
+
+        self.hero = hero.Hero(self.screen, self.world)
 
     def play(self):
         """Start Scroller program.
@@ -36,9 +55,16 @@ class Scroller(object):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
                     running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.vp = self.hero.move((-3, 0), self.vp)
+                    elif event.key == pygame.K_RIGHT:
+                        self.vp = self.hero.move((3, 0), self.vp)
 
             # Draw the scene
             self.screen.fill((0, 0, 0))
+            self.screen.blit(self.world, (-self.vp[0], self.vp[1]))
+            self.screen.blit(self.hero.image, self.hero.rect)
             pygame.display.flip()
 
 
