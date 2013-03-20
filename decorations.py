378a8ba0441b120d70f93aeec5f9be360a88968f
Marcus M. Darden <marcus.darden@cs.olivetcollege.edu>
2013-03-20 02:08:23 -0400

    Formatting and cleanup.

--- decorations.py
+++ decorations.py
@@ -20,7 +20,7 @@ def new_cloud():
     circles = [((i * spc),
                 random.randint(-spc / 2, spc / 2),
                 random.randint(minr, maxr))
-                for i in range(num_circles)]
+               for i in range(num_circles)]
     print circles
 
     # Determine the surface size from the random circles
@@ -56,61 +56,3 @@ def new_cloud():
         pygame.draw.circle(image, (255, 255, 255), (x, y - shad), circ[2])
 
     return image
-
-# Cloud = class()
-
-# function Cloud:init()
-#     -- you can accept and set parameters here
-#     self.shapes = {}
-#     self.position = vec2(0,0)
-
-#     -- Generate random cloud
-#     numCircles = math.random(4, 5)
-#     spacing = 30
-
-#     for i = 1,numCircles do
-#         x = i * spacing - ((numCircles/2)*spacing)
-#         y = (math.random() - 0.5) * 30
-#         rad = math.random(spacing, 2*spacing)
-#         table.insert(self.shapes, {x=x, y=y, r=rad})
-#     end
-
-#     self.width = numCircles * spacing + spacing
-# end
-
-# function Cloud:isColliding(pos)
-#     startp = self.position.x - self.width/2
-#     endp = self.position.x + self.width/2
-
-#     if pos.x < endp and pos.x > startp and
-#        pos.y < (self.position.y + 30) and
-#        pos.y > (self.position.y + 10) then
-#         return true
-#     end
-
-#     return false
-# end
-
-# function Cloud:draw()
-#     pushStyle()
-#     pushMatrix()
-
-#     translate(self.position.x, self.position.y)
-
-#     noStroke()
-#     ellipseMode(RADIUS)
-#     fill(167, 190, 221, 255)
-
-#     for i,s in ipairs(self.shapes) do
-#         ellipse(s.x, s.y - 5, s.r)
-#     end
-
-#     fill(255, 255, 255, 255)
-
-#     for i,s in ipairs(self.shapes) do
-#         ellipse(s.x, s.y + 5, s.r)
-#     end
-
-#     popMatrix()
-#     popStyle()
-# end
